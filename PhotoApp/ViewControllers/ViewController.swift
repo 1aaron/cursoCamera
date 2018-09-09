618191fc1dbed345a6750af74cd8bcb9e3144607
aaron <aaronnek1@gmail.com>
2018-09-08 22:45:30 -0500

    added pickerChooser

--- PhotoApp/ViewControllers/ViewController.swift
+++ PhotoApp/ViewControllers/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 import AVFoundation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var image: UIImage? = nil
+    let imagePicker = UIImagePickerController()
     @IBOutlet weak var imageView: UIImageView!
+    //obtener la imágen proveniente del otro ViewController (después de probar el picker por si solo)
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         if image != nil {
             imageView.image = image
         }
+    }
+    
+    //presentar el controlador para seleccionar fotografías
+    @IBAction func openChooser(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
 
